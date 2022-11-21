01d298af1864e6964827d2e337aa6c1c7abfbdbb
AdnanTurkay <adntrky@gmail.com>
2022-11-21 09:45:09 +0000

    Added a generic user model to build specific user models on

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+
+# A generic user class.
+class User(AbstractUser):
+    # Add user types as necessary.
+    USER_TYPE_CHOICES = (
+      (1, 'student'),
+      (2, 'adult'),
+      (3, 'teacher'),
+      (4, 'admin'),
+      (5, 'superadmin'),
+    )
+
+    # Define attributes.
+    # All valid users must have a user type, username, first name, last name, and an email.
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    username = models.CharField(
+        max_length=16,
+        unique=True, validators=[RegexValidator(
+            regex=r'^\w{3,16}$',
+            message='Username must consist of three to sixteen alphanumericals'
+        )]
+    )
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
