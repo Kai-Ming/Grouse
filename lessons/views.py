15ee916c91ec78b131966466657973722febb97f
Fergan Yalim <fergan.yalim@kcl.ac.uk>
2022-11-26 04:28:19 +0000

    Implement back end for the student registration page so that a new user is created whenever the form is filled and submitted with correct information

--- lessons/views.py
+++ lessons/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from .forms import SignUpForm
+from django.shortcuts import redirect,render
+from .forms import StudentSignUpForm
 from .forms import LogInForm
 
 def student_sign_up(request):
-    form = SignUpForm()
+    if request.method == "POST":
+        form = StudentSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("user_page")
+    else:
+        form = StudentSignUpForm()
     return render(request, 'student_sign_up.html', {'form': form})
 
 def log_in(request):
