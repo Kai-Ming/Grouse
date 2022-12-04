c00a090ef1d616853e13dac1b8b7e0677682125a
Abhishek Rao Chimbili <abhishekraochimbili@gmail.com>
2022-12-04 23:40:16 +0000

    Update test_forms.py

--- lessons/tests/models/test_forms.py
+++ lessons/tests/models/test_forms.py
@@ -3,3 +3,9 @@ from django.test import TestCase
 from lessons.forms import StudentSignUpForm, LogInForm
 from lessons.models import User
 
+class SignupFormTestCase:
+    # If it works for student signup it will work for adult cause both classes have the same attributes
+    def setUp(self):
+        self.form_input = {'first_name': 'John', 'last_name': 'Doe', 'username': 'jd123', 'email': 'jd123@gmail.com'}
+
+    
