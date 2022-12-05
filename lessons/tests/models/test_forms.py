f11139a32dea387fef5f2fb5fe97a535d34c511d
Abhishek Rao Chimbili <abhishekraochimbili@gmail.com>
2022-12-05 00:05:39 +0000

    Completed writing tests for login form

--- lessons/tests/models/test_forms.py
+++ lessons/tests/models/test_forms.py
@@ -53,3 +53,60 @@ class SignupFormTestCase:
 class LoginFormTestCase:
     def setUp(self):
         self.form_input = {'Username':'jd123', 'password':'password0'}
+
+    def test_form_has_required_fields(self):
+        form = LogInForm()
+        self.assertIn('username', form.fields)
+        self.assertIn('password', form.fields)
+        password_field = form.fields['password']
+        self.assertTrue(isinstance(password_field.widget,forms.PasswordInput))
+
+    def test_form_accepts_valid(self):
+        form = LogInForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_blank_username(self):
+        self.form_input['username'] = ''
+        form = LogInForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+    
+    def test_blank_username_authentication(self):
+        form_input = {'username': '', 'password': 'Password123'}
+        form = LogInForm(data=form_input)
+        user = form.get_user()
+        self.assertEqual(user, None)
+
+    def test_blank_password(self):
+        self.form_input['password'] = ''
+        form = LogInForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_blank_password_authentication(self):
+        form_input = {'username': 'jd123', 'password': ''}
+        form = LogInForm(data=form_input)
+        user = form.get_user()
+        self.assertEqual(user, None)
+
+    def test_form_incorrect_username(self):
+        self.form_input['username'] = 'ja'
+        form = LogInForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_incorrect_password(self):
+        self.form_input['password'] = 'pwd'
+        form = LogInForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_authenticate_valid_user(self):
+        fixture = User.objects.get(username='jd123')
+        form_input = {'username': 'jd123', 'password': 'Password123'}
+        form = LogInForm(data=form_input)
+        user = form.get_user()
+        self.assertEqual(user, fixture)
+
+    def test_invalid_credentials(self):
+        form_input = {'username': 'jd123', 'password': 'WrongPassword123'}
+        form = LogInForm(data=form_input)
+        user = form.get_user()
+        self.assertEqual(user, None)
+
